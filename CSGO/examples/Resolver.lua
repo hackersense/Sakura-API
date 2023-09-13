2d81128485730b61541f071c884a4bb0590ad9f5
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2023-09-13 13:13:07 +0800

    Update Resolver.lua

--- CSGO/examples/Resolver.lua
+++ CSGO/examples/Resolver.lua
@@ -1,6 +1,6 @@
 local function on_resolver(resolver_info)
   local player = entitylist.get_player_by_index(resolver_info.target_index)
-  resolver_info.current_animation = player:get_eye_angles().y + player:get_max_desync_delta()
+  resolver_info.set_current_animation(player:get_eye_angles().y + player:get_max_desync_delta())
 end
 -- this just joke, don't trust this lmao
 cheat.register_callback("on_resolver", on_resolver)
